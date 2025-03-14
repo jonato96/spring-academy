c583ec8f36260e43e4220b249d90d75b9d304217
jonato96 <jonato96s@gmail.com>
2025-03-13 20:47:45 -0500

    Implementing Post Lab

--- cashcard/src/main/java/spring/academy/cashcard/CashCardController.java
+++ cashcard/src/main/java/spring/academy/cashcard/CashCardController.java
@@ -1,11 +1,10 @@
 package spring.academy.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -28,5 +27,14 @@ public class CashCardController {
         }
     }
 
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
 
 }
